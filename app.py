09d00996b0e3433e6ceed0f9cefef67a3b7d95c5
meilana <74287788+MSiswanto@users.noreply.github.com>
2025-06-19 22:32:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,16 @@ st.markdown("""
 st.markdown("<div class='main-title'>👨‍💻 Data Science Portfolio</div>", unsafe_allow_html=True)
 st.markdown("<div class='subtitle'>Explore our projects and latest insights in Data Science and AI</div>", unsafe_allow_html=True)
 
+# Tampilkan judul sidebar
+st.sidebar.markdown('<div class="sidebar-title">📁 Navigasi</div>', unsafe_allow_html=True)
+with st.sidebar:
+    st.markdown('<div class="sidebar-box">', unsafe_allow_html=True)
+    #menu = st.radio("", ["🏠 Home", "🔍 Prediksi", "📊 Visualisasi"])
+    menu = st.sidebar.radio("Navigasi", ["🏠 Home", "📂 Projects", "📰 Blog", "📞 Contact"])
+    st.markdown('</div>', unsafe_allow_html=True)
+
 # Menu Navigasi
-menu = st.sidebar.radio("Navigasi", ["🏠 Home", "📂 Projects", "📰 Blog", "📞 Contact"])
+#menu = st.sidebar.radio("Navigasi", ["🏠 Home", "📂 Projects", "📰 Blog", "📞 Contact"])
 
 # Halaman Home
 if menu == "🏠 Home":
